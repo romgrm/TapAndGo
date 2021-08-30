76c9b4c6d7ac2ee6412d4b429552ec588c7e04ae
Romain <romaingreaume@gmail.com>
2021-08-30 20:36:22 +0200

    Add navigation between screens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { MapScreen } from "./src/screens/mapScreen";
+import { InformationsScreen } from "./src/screens/informationsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MapScreen} />
+        <Stack.Screen name="Infos" component={InformationsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/mapScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+import {Text, Button} from 'react-native'
+
+interface Props {
+    navigation: any; 
+}
+
+export function MapScreen(props: Props) {
+    return (
+        <View>
+
+            <Text>Map Screen</Text>
+            <Button onPress={() => props.navigation.navigate('Infos')} title="go"/>
+        </View>
+        
+    )
+}
