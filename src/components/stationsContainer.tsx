b04acc3b13dd041585b99b54b35e7c171bab54c6
Romain <romaingreaume@gmail.com>
2021-09-01 14:05:23 +0200

    [Feature]_Display stations points in the map

--- src/components/stationsContainer.tsx
+++ src/components/stationsContainer.tsx
@@ -6,13 +6,32 @@ import { RootState } from "../store/combineReducers";
 import { callApiStation } from "../store/stations/actionStation";
 import MapboxGL from "@react-native-mapbox-gl/maps";
 import { Environments } from "../config/environment";
+import { Logger } from "@react-native-mapbox-gl/maps";
 
 MapboxGL.setAccessToken(Environments.development.MAP_TOKEN);
+MapboxGL.setConnected(true);
 
+Logger.setLogCallback((log) => {
+  const { message } = log;
+
+  // expected warnings - see https://github.com/mapbox/mapbox-gl-native/issues/15341#issuecomment-522889062
+  if (
+    message.match("Request failed due to a permanent error: Canceled") ||
+    message.match("Request failed due to a permanent error: Socket Closed")
+  ) {
+    return true;
+  }
+  return false;
+});
 export default function stationsContainer() {
   const stations = useSelector((state: RootState) => state.stations.stations);
   const dispatch = useDispatch();
-  const coordinates = [-122.4324, 37.78825];
+  const coordinates = [2.213749, 46.227638];
+
+  /**
+   * Only 10 stations selected for the demo (performs reasons)
+   */
+  const reduceStations = stations.slice(0, 10);
 
   useEffect(() => {
     dispatch(callApiStation());
@@ -21,15 +40,14 @@ export default function stationsContainer() {
     <>
       <View style={styles.page}>
         <View style={styles.container}>
-          {/* <Text>hello, comment tu vas ? ça va bien</Text> */}
-          {/* {stations.map(val => <Text key={val.name}>{val.status}</Text>)} */}
-          <MapboxGL.MapView style={styles.map}>
-            {/* <MapboxGL.Camera zoomLevel={16} centerCoordinate={coordinates} />
-
-            <MapboxGL.PointAnnotation
-              coordinate={coordinates}
-              id="pt-ann"
-            ></MapboxGL.PointAnnotation> */}
+          <MapboxGL.MapView style={styles.map} zoomEnabled>
+            <MapboxGL.Camera centerCoordinate={coordinates} />
+            {reduceStations.map((val) => (
+              <MapboxGL.PointAnnotation
+                coordinate={[val.position.longitude, val.position.latitude]}
+                id="pt-ann"
+              ></MapboxGL.PointAnnotation>
+            ))}
           </MapboxGL.MapView>
         </View>
       </View>
