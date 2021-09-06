22fe2e0cae52d8b1fc16794c0fd81f31d7be9cb1
Romain <romaingreaume@gmail.com>
2021-09-06 13:01:19 +0200

    [Clean & ReadMe & Test]_Add readMe , clean some stuffs, test failed

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TapAndGo
-A real-time bicycle rental application, develop with ReactNative
+A real-time bicycle rental application, develop with ReactNative and JC Decaux API
+## Run it locally 
+
+1. Clone the project TapandGo
+```
+git clone https://github.com/romgrm/TapAndGo.git
+```
+2. Install dependencies 
+```
+cd TapandGo
+yarn install | npm install
+```
+3. Run the app 
+```
+yarn android | npm android
+``` 
+
+You can run the app on your devices (android) or emulator.
--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import store from "../src/store/store";
-import {Provider} from "react-redux";
-import { render } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { render, cleanup, fireEvent } from "@testing-library/react-native";
 import App from "../App";
+import RootNavigation from '../src/navigation/rootNavigation'
+import "react-native-gesture-handler/jestSetup";
+import { NavigationContainer } from "@react-navigation/native";
+
+afterEach(cleanup);
 
 jest.mock("react-native-reanimated", () => {
   const Reanimated = require("react-native-reanimated/mock");
 
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
   Reanimated.default.call = () => {};
 
   return Reanimated;
 });
 
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
 
 // jest.mock('react-navigation', () => ({
@@ -23,19 +32,52 @@ jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
 //   }));
 
 describe("<App />", () => {
-  const dispatch = jest.fn();
-  store.dispatch = dispatch;
-  const fetchData = jest.fn();
+  // configureStore expects a list of middlewares
+  const mockStore = configureStore([]);
 
-  beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <App />
+  it("should render", () => {
+    const storeMock = mockStore(store);
+    const {getByTestId} = render(
+      <Provider store={storeMock}>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
       </Provider>
     );
+    // const textComponent = rendered.getByTestId('text');
+    const tree = getByTestId('test')
+    expect(tree?.props.children).toBe(3);
   });
-  it("has 1 child", () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toBe(1);
-  });
+
+  // it('should dispatch increment action', () => {
+  //   const store = mockStore({count: 5});
+  //   const rendered = render(
+  //     <Provider store={store}><Counter /></Provider>
+  //   );
+  //   const buttonComponent = rendered.getByTestId('button');
+
+  //   fireEvent(buttonComponent, 'press');
+
+  //   // This will return all actions dispatched on this store
+  //   const actions = store.getActions();
+  //   expect(actions.length).toBe(1);
+  //   expect(actions[0].type).toEqual('INCREMENT');
+  // });
 });
+// describe("<App />", () => {
+//   const dispatch = jest.fn();
+//   store.dispatch = dispatch;
+//   const fetchData = jest.fn();
+
+//   beforeEach(() => {
+//     render(
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     );
+//   });
+//   it("has 1 child", () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree).toBe(1);
+//   });
+// });
